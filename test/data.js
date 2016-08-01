c9757c486246f853c26f5f74b88c7a72c59b987d
Diego Silva <dsilva2401@gmail.com>
2016-07-31 21:08:18 -0500

    Auth tests defined

---
+++ test/data.js
@@ -0,0 +1,17 @@
+
+// Init data object
+	var data = {};
+
+// Define data
+
+	// Person
+		data.person = {};
+		data.person.name = 'Name';
+		data.person.lastname = 'Lastname';
+		data.person.sex = 'male';
+		data.person.birthday = new Date();
+		data.person.email = 'user@domain.com';
+		data.person.password = 'password123';
+
+// Exports
+	module.exports = data;
---
+++ test/services/auth.spec.js
@@ -0,0 +1,31 @@
+
+// Imports
+	var expect = require("chai").expect;
+
+// Setup
+	describe('Authentication', function () {
+		
+		describe('Register', function () {
+			
+			it('Register new user', function () {});
+			it('Register an already registered user', function () {});
+			it('Register with invalid data', function () {});
+			it('Register with missing data', function () {});
+
+		});
+
+		describe('Login', function () {
+			
+			it('Login with invalid credentials', function () {});
+			it('Login with valid credentials', function () {});
+			it('Login with missing fields', function () {});
+
+		});
+
+		describe('Logout', function () {
+			
+			it('End session', function () {});
+
+		});
+
+	});
